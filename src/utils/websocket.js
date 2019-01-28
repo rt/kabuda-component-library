a2b9a685110e645dfd27b8368dd1eaf8bdbac916
rtsunoda <ryantsunoda@gmail.com>
2019-01-28 06:03:06 +0000

    add websocket, can implement later...

---
+++ src/utils/websocket.js
@@ -0,0 +1,19 @@
+
+      // window.addEventListener("load", function(){ //when page loads
+      //   var lightbox = document.getElementById("light");
+      //   lightbox.addEventListener("change", function() { //add event listener for when checkbox changes
+      //     socket.emit("light", Number(this.checked)); //send button status to server (as 1 or 0)
+      //   });
+      // });
+      // Create WebSocket connection.
+      const socket = new WebSocket('ws://student2.local:8081');
+
+      // Connection opened
+      socket.addEventListener('open', function (event) {
+        socket.send('Hello Server!');
+      });
+
+      // Listen for messages
+      socket.addEventListener('message', function (event) {
+        console.log('Message from server ', event.data);
+      });
